287b6e3b409f1adfd77957171d02477511136764
ORavid25 <oravid12@gmail.com>
2022-01-12 17:25:50 +0200

    ....

--- server/index.js
+++ server/index.js
@@ -3,10 +3,30 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Calculator NodeJS API ",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./index.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(
   cors({
@@ -16,7 +36,55 @@ app.use(
 );
 
 
-// the POST method accepts 3 arguments : n1,n2,operator and return the result for calculation.
+/**
+ * @swagger
+ * components:
+ *    schema:
+ *       calc: 
+ *           type: object
+ *           properties:
+ *               n1: 
+ *                   type: integer
+ *               n2: 
+ *                   type: integer
+ *               operator: 
+ *                   type: string
+ * 
+ */
+
+
+
+
+
+/**
+ * @swagger
+ * /api:
+ *    get:
+ *        summary: used to check if get method is working or not
+ *        description: sending message from nodejs server
+ *        responses:
+ *            200:
+ *                description: this api is used to fetch data from nodejs server
+ *
+ */
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+/**
+ * @swagger
+ * /calc:
+ *    post:
+ *      summery: used to insert data to make calculation between 2 numbers
+ *      description: 'the POST method accepts 3 arguments : n1,n2,operator and  the result for calculation.'
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     $ref: '#components/schema/calc'
+ */
+
 app.post(`/calc`, (req, res) => {
   var n1 = parseInt(req.body.firstNum);
   var n2 = parseInt(req.body.secondNum);
@@ -43,15 +111,8 @@ app.post(`/calc`, (req, res) => {
   }
   console.log("result for calc is", result);
   res.json(result);
-  
-});
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
 });
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
 });
-
-
